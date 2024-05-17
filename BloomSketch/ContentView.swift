d22b28146b7a9364807b13d2e8cb0e537dab57bb
willyc2004 <willy.cahyadi25@gmail.com>
2024-05-17 09:44:13 +0700

    Buat CustomNavigationLink, biar isa dipakai langsung panggil.

--- BloomSketch/ContentView.swift
+++ BloomSketch/ContentView.swift
@@ -6,163 +6,18 @@
 //
 
 import SwiftUI
-import PencilKit
 
 struct ContentView: View {
-    @State private var sproutName = ""
-    @State var canvas = PKCanvasView()
-//    @State private var isDrawingViewActive = false
-    @State var isDraw = true
-    @State var color : Color = .black
-    @State var type: PKInkingTool.InkType = .pencil
-    @State var colorPicker = false
-    @State private var action: Int? = 0
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                VStack {
-                    Image("BloomSketch_LogoWhite")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 240, height: 240)
-                        .foregroundColor(.red)
-                    
-                    Spacer()
-                    
-                    VStack(spacing: 2) {
-                        Text("Name Your New Sprout")
-                            .font(.system(size: 20, weight: .bold))
-                            .foregroundColor(Color(hex: 0x1B3F2E))
-                        
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 16)
-                                .fill(Color.white)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 16)
-                                        .stroke(Color.gray, lineWidth: 1)
-                                )
-                                .frame(height: 56)
-                            
-                            TextField("CutiePatotie", text: $sproutName)
-                                .padding(.horizontal)
-                        }
-                        .frame(width: 300)
-                        .padding(.top, 16)
-                        
-                        //button yang bener ini
-                        //                        Button(action: createSprout) {
-                        //                            Text("Start")
-                        //                                .font(.headline)
-                        //                                .foregroundColor(.white)
-                        //                                .padding(.vertical, 12)
-                        //                                .padding(.horizontal, 64)
-                        //                                .background(Color(hex: 0x1B3F2E))
-                        //                                .cornerRadius(16)
-                        //                        }
-                        //                        .padding(.top, 24)
-                        
-                        //tes buton buat ke drawing fitur.
-                        Button(action: {self.action = 1}) {
-                            Text("Start")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                                .padding(.vertical, 12)
-                                .padding(.horizontal, 64)
-                                .background(Color(hex: 0x1B3F2E))
-                                .cornerRadius(16)
-                        }
-                        .padding(.top, 24)
-                    }
-                    
-                    NavigationLink(
-                        destination: DrawingView(canvas: $canvas, isDraw: $isDraw, type: $type, color: $color)
-                            .navigationTitle("Drawing")
-                            .navigationBarTitleDisplayMode(.inline)
-                            .navigationBarItems(
-                                leading: Button(action: {
-                                    // Save image action
-                                    SaveImage()
-                                }) {
-                                    EmptyView()
-                                },
-                                trailing: HStack(spacing: 15) {
-                                    ColorPicker("", selection: $color)
-                                        .padding()
-                                    
-                                    Button(action: {
-                                        isDraw = false
-                                    }) {
-                                        Image(systemName: "pencil.slash")
-                                            .font(.title)
-                                    }
-                                    
-                                    Menu {
-                                        Button(action: {
-                                            isDraw = true
-                                            type = .pencil
-                                        }) {
-                                            Label {
-                                                Text("Pencil")
-                                            } icon: {
-                                                Image(systemName: "pencil")
-                                            }
-                                        }
-                                        Button(action: {
-                                            isDraw = true
-                                            type = .pen
-                                        }) {
-                                            Label {
-                                                Text("Pen")
-                                            } icon: {
-                                                Image(systemName: "pencil.tip")
-                                            }
-                                        }
-                                        Button(action: {
-                                            isDraw = true
-                                            type = .marker
-                                        }) {
-                                            Label {
-                                                Text("Marker")
-                                            } icon: {
-                                                Image(systemName: "highlighter")
-                                            }
-                                        }
-                                        
-                                        
-                                    } label: {
-                                        Image("menu")
-                                            .resizable()
-                                            .frame(width: 22, height: 22)
-                                    }
-                                }
-                            ),
-                        //deprecated
-//                        isActive: $isDrawingViewActive,
-                        tag: 1,
-                        selection: $action,
-                        label: {
-                            EmptyView()
-                        }
-                    )
-                    .hidden()
-                    
-                }
-                Spacer()
-                
-                Image("BottomBarLeaves_Crop")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-            }
+        //lol
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
-    }
-    
-    func SaveImage() {
-        //get image from canvas
-        let image = canvas.drawing.image(from: canvas.drawing.bounds, scale: 1)
-        
-        //save to photos/albums
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        .padding()
     }
 }
 
--- BloomSketch/NewSprout_View.swift
+++ BloomSketch/NewSprout_View.swift
@@ -82,88 +82,10 @@ struct NewSprout_View: View {
                         }
                         .padding(.top, 24)
                     }
-                    NavigationLink(
-                        destination: DrawingView(canvas: $canvas, isDraw: $isDraw, type: $type, color: $color)
-                            .navigationTitle("Drawing")
-                            .navigationBarTitleDisplayMode(.inline)
-                            .navigationBarItems(
-//                                leading: Button(action: {
-//                                    // Save image action
-//                                    SaveImage()
-//                                }) {
-//                                    EmptyView()
-//                                },
-                                trailing: HStack(spacing: 1) {
-                                    ColorPicker("", selection: $color)
-                                        .padding()
-                                    
-                                    Button(action: {
-                                        isDraw = false
-                                    }) {
-                                        Image(systemName: "pencil.slash")
-                                            .font(.title)
-                                    }
-                                    
-                                    Menu {
-                                        Button(action: {
-                                            isDraw = true
-                                            type = .pencil
-                                        }) {
-                                            Label {
-                                                Text("Pencil")
-                                            } icon: {
-                                                Image(systemName: "pencil")
-                                            }
-                                        }
-                                        Button(action: {
-                                            isDraw = true
-                                            type = .pen
-                                        }) {
-                                            Label {
-                                                Text("Pen")
-                                            } icon: {
-                                                Image(systemName: "pencil.tip")
-                                            }
-                                        }
-                                        Button(action: {
-                                            isDraw = true
-                                            type = .marker
-                                        }) {
-                                            Label {
-                                                Text("Marker")
-                                            } icon: {
-                                                Image(systemName: "highlighter")
-                                            }
-                                        }
-                                        
-                                        Button(action: {
-                                            SaveImage()
-                                        }) {
-                                            Label {
-                                                Text("Save to Photos")
-                                            } icon: {
-                                                Image(systemName: "highlighter")
-                                            }
-                                        }
-                                        
-                                        
-                                        
-                                    } label: {
-                                        Image("menu")
-                                            .resizable()
-                                            .frame(width: 22, height: 22)
-                                    }
-                                }
-                            ),
-                        tag: 1,
-                        selection: $action,
-                        label: {
-                            EmptyView()
-                        }
-                    )
-                    .hidden()
-                    
+                    CustomNavigationLink(canvas: $canvas, isDraw: $isDraw, type: $type, color: $color, action: $action)
                 }
+                
+                
                 Spacer()
                 
                 Image("BottomBarLeaves")
@@ -172,13 +94,6 @@ struct NewSprout_View: View {
                     .frame(maxWidth: .infinity, alignment: .bottomLeading)
             }
         }
-    }
-    func SaveImage() {
-        //get image from canvas
-        let image = canvas.drawing.image(from: canvas.drawing.bounds, scale: 1)
-        
-        //save to photos/albums
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
 }
 
