348030fa4003354f59606c9140235ce6e7f9bd7e
Lie, Samuel Miracle Kristanto <114904516+AttraqtiA@users.noreply.github.com>
2024-06-06 15:31:37 +0700

    NewSprout responsive

--- BloomSketch/OpeningViews/NewSprout_View.swift
+++ BloomSketch/OpeningViews/NewSprout_View.swift
@@ -23,87 +23,91 @@ struct NewSprout_View: View {
                     ZStack {
                         Color(hex: 0xD5E4DD)
                             .ignoresSafeArea()
-                        
-                        VStack {
-                            Image("top_fluid")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: .infinity, alignment: .topTrailing)
-                                .edgesIgnoringSafeArea(.top)
+                        if UIDevice.current.userInterfaceIdiom == .phone {
                             
-                            Image("BloomSketch_LogoFull")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: geometry.size.width * 0.5, height: geometry.size.width * 0.4)
-                                .foregroundColor(.red)
-                            
-                            Spacer()
-                            
-                            VStack(spacing: geometry.size.height * 0.02) {
-                                Text("Name Your New Sprout")
-                                    .font(.system(size: geometry.size.width * 0.05, weight: .bold))
-                                    .foregroundColor(Color(hex: 0x1B3F2E))
+                            VStack {
+                                Image("top_fluid")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: .infinity, alignment: .topTrailing)
+                                    .edgesIgnoringSafeArea(.top)
                                 
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 16)
-                                        .fill(Color.white)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 16)
-                                                .stroke(Color.gray, lineWidth: 1)
-                                        )
-                                        .frame(height: geometry.size.height * 0.07)
-
-                                    TextField("CutiePatotie", text: $sproutName)
-                                        .padding(.horizontal)
-                                        .font(.system(size: geometry.size.width * 0.03, weight: .bold))
-                                }
-                                .frame(width: geometry.size.width * 0.6)
-                                .padding(.top, geometry.size.height * 0.02)
-                                
-                                Button(action: {
-                                    if sproutName != "" {
-                                        navigateToTreeHome = true
-                                        tree.name = sproutName
-                                        tree.namedStatus = true
-                                        // Save changes to the context
-                                        try? modelContext.save()
-                                    } else {
-                                        // Show alert
-                                        showAlert.toggle()
-                                    }
-                                }) {
-                                    Text("Plant")
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                        .padding(.vertical, geometry.size.height * 0.02)
-                                        .padding(.horizontal, geometry.size.width * 0.15)
-                                        .background(Color(hex: 0x1B3F2E))
-                                        .cornerRadius(16)
-                                }
-                                .padding(.top, 16)
-                                .alert(isPresented: $showAlert) {
-                                    Alert(
-                                        title: Text("Empty Name <3"),
-                                        message: Text("Please name your sprout!"),
-                                        dismissButton: .default(Text("OK"))
-                                    )
-                                }
-                                
-                                NavigationLink(destination: ContentView(), isActive: $navigateToTreeHome) {
-                                    EmptyView()
-                                }
+                                Image("BloomSketch_LogoFull")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: geometry.size.width * 0.8, height: geometry.size.width * 0.6)
+                                    .foregroundColor(.red)
                                 
                                 Spacer()
+                                
+                                VStack(spacing: geometry.size.height * 0.02) {
+                                    Text("Name Your New Sprout")
+                                        .font(.system(size: geometry.size.width * 0.05, weight: .bold))
+                                        .foregroundColor(Color(hex: 0x1B3F2E))
+                                    
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 16)
+                                            .fill(Color.white)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 16)
+                                                    .stroke(Color.gray, lineWidth: 1)
+                                            )
+                                            .frame(height: geometry.size.height * 0.07)
+                                        
+                                        TextField("", text: $sproutName)
+                                            .padding(.horizontal)
+                                            .font(.system(size: geometry.size.width * 0.03, weight: .bold))
+                                            .multilineTextAlignment(.center)
+
+                                    }
+                                    .frame(width: geometry.size.width * 0.6)
+                                    .padding(.top, geometry.size.height * 0.03)
+                                    
+                                    Button(action: {
+                                        if sproutName != "" {
+                                            navigateToTreeHome = true
+                                            tree.name = sproutName
+                                            tree.namedStatus = true
+                                            // Save changes to the context
+                                            try? modelContext.save()
+                                        } else {
+                                            // Show alert
+                                            showAlert.toggle()
+                                        }
+                                    }) {
+                                        Text("Plant")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                            .padding(.vertical, geometry.size.height * 0.02)
+                                            .padding(.horizontal, geometry.size.width * 0.15)
+                                            .background(Color(hex: 0x1B3F2E))
+                                            .cornerRadius(16)
+                                    }
+                                    .padding(.top, 16)
+                                    .alert(isPresented: $showAlert) {
+                                        Alert(
+                                            title: Text("Empty Name <3"),
+                                            message: Text("Please name your sprout!"),
+                                            dismissButton: .default(Text("OK"))
+                                        )
+                                    }
+                                    
+                                    NavigationLink(destination: ContentView(), isActive: $navigateToTreeHome) {
+                                        EmptyView()
+                                    }
+                                    
+                                    Spacer()
+                                }
+                                
+                                Image("bottom_fluid")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: .infinity, alignment: .bottomLeading)
+                                    .offset(y: 34)
                             }
-                            
-                            Image("bottom_fluid")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: .infinity, alignment: .bottomLeading)
-                                .offset(y: 34)
                         }
                     }
-                    .navigationBarBackButtonHidden(true) // Hide back button
+                            .navigationBarBackButtonHidden(true) // Hide back button
                 }
             }
         }
