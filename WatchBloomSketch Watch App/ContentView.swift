0b72d6898a25b90adee5e5431d0b333df23eb700
Lie, Samuel Miracle Kristanto <114904516+AttraqtiA@users.noreply.github.com>
2024-06-07 21:49:45 +0700

    WatchOS ContentView adjust

--- WatchBloomSketch Watch App/ContentView.swift
+++ WatchBloomSketch Watch App/ContentView.swift
@@ -1,15 +1,6 @@
 import SwiftUI
 import UserNotifications
-struct ContentView3: View {
-    var body: some View {
-        VStack {
-            Text("Page 3")
-                .font(.largeTitle)
-                .foregroundColor(.blue)
-            Spacer()
-        }
-    }
-}
+
 struct ContentView: View {
     @State private var currentIndex = 0
     var body: some View {
